a0eca9ddc6a01698efb239c7bae8b2b91c1220b0
Coursera Learner <nobody@nowhere.nul>
2021-03-22 23:03:43 +0000

    Defined first 2 functions in cards.c

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -5,12 +5,23 @@
 
 
 void assert_card_valid(card_t c) {
-
-
+  assert((c.value >= 2 && c.value <= VALUE_ACE)); // Checking value
+  assert((c.suit >= SPADES && c.suit < NUM_SUITS)); // Checking suit
 }
 
 const char * ranking_to_string(hand_ranking_t r) {
-  return "";
+  switch(r) {
+  case STRAIGHT_FLUSH: return "Straight Flush";
+  case FOUR_OF_A_KIND: return "Four Of a Kind";
+  case FULL_HOUSE: return "Full House";
+  case FLUSH: return "Flush";
+  case STRAIGHT: return "Straight";
+  case THREE_OF_A_KIND: return "Three Of a Kind";
+  case TWO_PAIR: return "Two Pair";
+  case PAIR: return "Pair";
+  case NOTHING: return "Nothing";
+  default: return "Invalid ranking"; break;
+  }
 }
 
 char value_letter(card_t c) {
--- c2prj1_cards/my-test-main.c
+++ c2prj1_cards/my-test-main.c
@@ -1,5 +1,9 @@
 #include "cards.h"
+#include <stdio.h>
 
 int main(void) {
-
+  /*card_t card1;
+  card1.value = 2;
+  card1.suit = SPADES;*/
+  printf("Testing ranking .... %s\n", ranking_to_string(FLUSH));
 }
