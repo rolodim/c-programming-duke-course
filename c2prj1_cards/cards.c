2635a950b029e9ab0228c76c7a0189e50584398c
Coursera Learner <nobody@nowhere.nul>
2021-03-23 13:05:17 +0000

    Did all the functions and tested with input.txt file

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -25,25 +25,74 @@ const char * ranking_to_string(hand_ranking_t r) {
 }
 
 char value_letter(card_t c) {
-  return 'x';
+  char charValue;
+  if (c.value >= 2 && c.value <=10) {
+    charValue = c.value % 10 + '0';
+  }
+  else {
+    switch (c.value) {
+      case VALUE_JACK: charValue = 'J'; break;
+      case VALUE_QUEEN: charValue = 'Q'; break;
+      case VALUE_KING: charValue = 'K'; break;
+      case VALUE_ACE: charValue = 'A'; break;
+      default: charValue = 'e'; break;
+    }
+  }
+  return charValue;
 }
 
-
 char suit_letter(card_t c) {
-  return 'x';
-  
+  switch (c.suit) {
+  case SPADES: return 's';
+  case HEARTS: return 'h';
+  case DIAMONDS: return 'd';
+  case CLUBS: return 'c';
+  default: return 'e';
+  }
 }
 
 void print_card(card_t c) {
+  printf("%c%c", value_letter(c), suit_letter(c));
+}
 
+card_t value_from_letter(char value) {
+  card_t card;
+  switch (value) {
+  case 'J': card.value = VALUE_JACK; break;
+  case 'Q': card.value = VALUE_QUEEN; break;
+  case 'K': card.value = VALUE_KING; break;
+  case 'A': card.value = VALUE_ACE; break;
+  case '0': card.value = 10; break;
+  default: card.value = value - '0'; break;
+  }
+  return card;
+}
+
+card_t suit_from_letter(char suit) {
+  card_t card;
+  switch (suit) {
+  case 's': card.suit = SPADES; break;
+  case 'h': card.suit = HEARTS; break;
+  case 'd': card.suit = DIAMONDS; break;
+  case 'c': card.suit = CLUBS; break;
+  default: card.suit = NUM_SUITS; break;
+  }
+  return card;
 }
 
 card_t card_from_letters(char value_let, char suit_let) {
   card_t temp;
+  temp = value_from_letter(value_let);
+  temp = suit_from_letter(suit_let);
+  assert_card_valid(temp);
   return temp;
 }
 
 card_t card_from_num(unsigned c) {
   card_t temp;
+  if (c >= 0 && c < 52) {
+    temp.value = c % 13 + 2;
+    temp.suit = c / 13;
+  }
   return temp;
 }
--- c2prj1_cards/my-test-main.c
+++ c2prj1_cards/my-test-main.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 int main(void) {
-  /*card_t card1;
-  card1.value = 2;
-  card1.suit = SPADES;*/
-  printf("Testing ranking .... %s\n", ranking_to_string(FLUSH));
+  card_t card1;
+  card1 = card_from_num(5);
+  // printf("Testing ranking .... %s\n", ranking_to_string(FLUSH));
+  print_card(card1);
 }
