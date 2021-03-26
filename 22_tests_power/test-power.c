1bcf7d2f8e1c9d59eaaca5bc03bca2d08d8b750e
Coursera Learner <nobody@nowhere.nul>
2021-03-26 16:35:12 +0000

    Added print statements to test file

--- 22_tests_power/test-power.c
+++ 22_tests_power/test-power.c
@@ -30,17 +30,21 @@ int main(void) {
 
   if (power(c1.x, c1.y) != 1) {
     exit(EXIT_FAILURE);
+    printf("Test 1 failed!\n");
   };
   if (power(c2.x, c2.y) != -5) {
     exit(EXIT_FAILURE);
+    printf("Test 2 failed!\n");
   };
   if (power(c3.x, c3.y) != 1) {
     exit(EXIT_FAILURE);
+    printf("Test 1 failed!\n");
   };
   if (power(c4.x, c4.y) != 64) {
     exit(EXIT_FAILURE);
+    printf("Test 1 failed!\n");
   };
-
+  printf("All the tests are completed seccessfully!\n");
   return EXIT_SUCCESS;
   
 }
