3a7455f9b8eb8cb4742f53e9cb0461def91d6cd0
Coursera Learner <nobody@nowhere.nul>
2021-04-01 20:49:11 +0000

    Wrote a frequency counting functions

---
+++ 25_break_encr/breaker.c
@@ -0,0 +1,60 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+void counting(FILE * f, int * array) {
+  int c;
+  while ((c = fgetc(f)) != EOF) {
+    if (isalpha(c)) {
+      c = tolower(c);
+      c -= 'a';
+      c %= 26;
+      array[c]++;
+    }
+  }
+}
+
+int maxValue(int * array, int n) {
+  int max = array[0];
+  for (int i = 0; i < n; i++) {
+    if (array[i] > max) {
+      max = array[i];
+    }
+  }
+  return max;
+}
+
+int maxIndex(int * array, int m, int n) {
+  for (int i = 0; i < n; i++) {
+    if (array[i] == m) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+
+int main(int argc, char ** argv) {
+  const int ALP = 26;
+  if (argc != 2) {
+    fprintf(stderr, "Usage: decrypt file not found\n");
+    return EXIT_FAILURE;
+  }
+  FILE * f = fopen(argv[1], "r");
+  if (f == NULL) {
+    perror("Could not open file");
+    return EXIT_FAILURE;
+  }
+  int chars[26] = { 0 };
+  counting(f, chars);
+  int key;
+  key = maxIndex(chars, maxValue(chars, ALP), ALP);
+  printf("%d\n", key);
+
+  if (fclose(f) != 0) {
+    perror("Failed to clode the input file!");
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
