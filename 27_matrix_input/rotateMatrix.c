2e2d41f2df098eb4cd013837fdb6ce04e6041644
Coursera Learner <nobody@nowhere.nul>
2021-04-02 20:41:42 +0000

    Wrote main function which read strings from an input file

---
+++ 27_matrix_input/rotateMatrix.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "rotate.c"
+
+#define SIZE 10
+
+void rotate(char matrix[10][10]);
+
+int main(int argc, char ** argv) {
+
+  if (argc != 2) {
+    fprintf(stderr, "Input file is not given!\n");
+    return EXIT_FAILURE;
+  }
+  FILE * f = fopen(argv[1], "r");
+  if (f == NULL) {
+    fprintf(stderr, "The file is empty!\n");
+    return EXIT_FAILURE;
+  }
+
+  char matrix[SIZE][SIZE+2];
+  char result[SIZE][SIZE];
+  int i = 0;
+  while (fgets(matrix[i], SIZE + 2, f) != NULL) {
+    if (((strchr(matrix[i], '\n') - matrix[i]) != matrix[i][SIZE])) {
+      fprintf(stderr, "Incorrect line in the input file - %d\n", i);
+      return EXIT_FAILURE;
+    }
+    //printf("Row %d \\n found in %d position.\n", i, (int)(strchr(matrix[i], '\n') - matrix[i]));
+    for (int j = 0; j < SIZE; j++) {
+      result[i][j] = matrix[i][j];
+    }
+    i++;
+  }
+  rotate(result);
+  for (int i = 0; i < SIZE; i++) {
+    for (int j = 0; j < SIZE; j++) {
+      printf("%c", result[i][j]);
+    }
+    printf("\n");
+  }
+      
+
+  if (fclose(f) != 0) {
+    fprintf(stderr, "Failed to close the input file!\n");
+    return EXIT_FAILURE;
+  }
+  
+  return EXIT_SUCCESS;
+}
