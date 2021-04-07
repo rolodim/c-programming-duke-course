06746d39f07b699003572ec923d60255945e3511
Coursera Learner <nobody@nowhere.nul>
2021-04-07 09:10:36 +0000

    First try to grade

--- 32_kvs/kv.c
+++ 32_kvs/kv.c
@@ -3,20 +3,56 @@
 #include <string.h>
 #include "kv.h"
 
-
-
 kvarray_t * readKVs(const char * fname) {
-  //WRITE ME
+  kvarray_t * map = malloc(sizeof(*map));
+  char * line = NULL;
+  map->elements = NULL;
+  size_t s;
+  size_t i = 0;
+  FILE * f = fopen(fname, "r");
+  if (f == NULL) {
+    fprintf(stderr, "File (%s) is empty!\n", fname);
+    return NULL;
+  }
+  while (getline(&line, &s, f) >= 0) {
+    map->elements = realloc(map->elements, (i+1) * sizeof(map->elements));
+    map->elements[i] = malloc(sizeof(*map->elements[i]));
+    size_t index = strchr(line, '=') - line;
+    line[index] = '\0';
+    map->elements[i]->key = line;
+    map->elements[i]->value = &line[index+1];
+    line = NULL;
+    i++;
+  }
+  free(line);
+  map->size = i;
+  if (fclose(f) != 0) {
+    fprintf(stderr, "Failed to close the input file!\n");
+    return NULL;
+  }
+  return map;
 }
 
 void freeKVs(kvarray_t * pairs) {
-  //WRITE ME
+  for (size_t i = 0; i < pairs->size; i++) {
+    free(pairs->elements[i]->key);
+    free(pairs->elements[i]);
+  }
+  free(pairs->elements);
+  free(pairs);
 }
 
 void printKVs(kvarray_t * pairs) {
-  //WRITE ME
+  for (size_t i = 0; i < pairs->size; i++) {
+    printf("key = '%s' value = '%s'\n", pairs->elements[i]->key, pairs->elements[i]->value);
+  }
 }
 
 char * lookupValue(kvarray_t * pairs, const char * key) {
-  //WRITE ME
+  for (size_t i = 0; i < pairs->size; i++) {
+    if (strcmp(pairs->elements[i]->key, key) == 0) {
+      return pairs->elements[i]->value;
+    }
+  }
+  return NULL;
 }
