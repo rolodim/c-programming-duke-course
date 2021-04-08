abee50eb6c304e00262da0505c4c9364e0040bab
Coursera Learner <nobody@nowhere.nul>
2021-04-08 16:21:32 +0000

    Fixed allocation memory for add_card-To

--- c4prj1_deck/test.c
+++ c4prj1_deck/test.c
@@ -8,6 +8,12 @@ deck_t * createEmptyDeck(void);
 int main(void) {
   deck_t * d;
   d = createEmptyDeck();
+  for (size_t i = 0; i < 51; i++) {
+    add_card_to(d, card_from_num(i));
+  }
+  add_empty_card(d);    
   print_hand(d);
+  free_deck(d);
+  printf("\n");
   return EXIT_SUCCESS;
 }
