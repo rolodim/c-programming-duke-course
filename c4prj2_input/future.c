eab666ecd0089e37c0dc6b8f58aee555bfefcef8
Coursera Learner <nobody@nowhere.nul>
2021-04-09 08:49:06 +0000

    Replaced assignment of the card in future_cards_from_deck

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -19,15 +19,17 @@ void future_cards_from_deck(deck_t * deck, future_cards_t * fc) {
   size_t c = 0;
   for (size_t i = 0; i < fc->n_decks; i++) {
     if (fc->decks[i].n_cards > 0) {
-      //if (deck->n_cards < c) {
+      if (deck->n_cards > c) {
 	for (size_t j = 0; j < fc->decks[i].n_cards; j++) {
-	  fc->decks[i].cards[j] = deck->cards[c];
+	  fc->decks[i].cards[j]->value = deck->cards[c]->value;
+	  fc->decks[i].cards[j]->suit = deck->cards[c]->suit;
 	  c++;
 	}
-	//}
-	//else {
-	//fprintf(stderr, "Too few cards in the deck!\n");
-	//}
+      }
+      else {
+	fprintf(stderr, "Too few cards in the deck!\n");
+	break;
+      }
     }
   }
 }
