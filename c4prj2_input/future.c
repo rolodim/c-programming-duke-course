2dbc58e313b8d3e8db8e72be4113ab0e8f978711
Coursera Learner <nobody@nowhere.nul>
2021-04-09 09:15:46 +0000

    old if/else and return

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -17,7 +17,7 @@ void add_future_card(future_cards_t * fc, size_t index, card_t * ptr) {
 
 void future_cards_from_deck(deck_t * deck, future_cards_t * fc) {
   size_t c = 0;
-  if (deck->n_cards > fc->n_decks) {
+  if (deck->n_cards > c) {
     for (size_t i = 0; i < fc->n_decks; i++) {
       if (fc->decks[i].n_cards > 0) {
 	for (size_t j = 0; j < fc->decks[i].n_cards; j++) {
