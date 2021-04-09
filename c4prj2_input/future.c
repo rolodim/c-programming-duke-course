18ae3faf9040778057813bf560a4a8133445baa6
Coursera Learner <nobody@nowhere.nul>
2021-04-09 09:09:05 +0000

    return in input.c and printf instead fprintf

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -17,10 +17,11 @@ void add_future_card(future_cards_t * fc, size_t index, card_t * ptr) {
 
 void future_cards_from_deck(deck_t * deck, future_cards_t * fc) {
   size_t c = 0;
-  //if (deck->n_cards < fc->n_decks) {
-  //  fprintf(stderr, "Too few cards in the deck!\n");
-  //}
-  //else {
+  if (deck->n_cards < fc->n_decks) {
+    printf("Too few cards in the deck!\n");
+    return;
+  }
+  else {
     for (size_t i = 0; i < fc->n_decks; i++) {
       if (fc->decks[i].n_cards > 0) {
 	for (size_t j = 0; j < fc->decks[i].n_cards; j++) {
@@ -30,5 +31,5 @@ void future_cards_from_deck(deck_t * deck, future_cards_t * fc) {
 	}
       }
     }
- //}
+ }
 }
